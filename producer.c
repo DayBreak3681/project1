50af664e75a21c08fb07b29e63051985c0d25111
Lennice Bolton <76896866+DayBreak3681@users.noreply.github.com>
2023-11-07 23:55:40 -0500

    Update producer.c

--- producer.c
+++ producer.c
@@ -19,21 +19,24 @@ void *producer(void *args)
     size_t count = 0;
     int fd;
     struct shmbuf *shmp;
-    fd = shm_open("/xyzabc", O_CREAT | O_EXCL | O_RDWR, 0600);
-    ftruncate(fd, sizeof(struct shmbuf));
-    shmp = mmap(NULL, sizeof(*shmp), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    sem_init(&shmp->semEmpty, 1, 0);
-    sem_init(&shmp->semFull, 1, 2);
-    while (1)
+    fd = shmget(HELLO,sizeof(struct shmbuf), 0600|IPC_CREAT); //shared memory
+
+    shmp = shmat(fd,NULL,0);
+    sem_init(&shmp->semEmpty, 1, 5); 
+    sem_init(&shmp->semFull, 1, 0);
+    int x = 0;
+    for (int i = 0; i < 5; ++i)//pushing 5 elements into buffer
     {
-        // Produce
-        int x = rand() % 100;
+    
+        
         //sleep(1);
 
         // Add to the buffer
         sem_wait(&shmp->semFull);
         shmp->buffer[count] = x;
+        printf("Produced: %d\n", shmp->buffer[count]);
         count++;
+        ++x;
         sem_post(&shmp->semEmpty);
     }
 
@@ -44,13 +47,13 @@ int main(int argc, char *argv[])
     srand(time(NULL));
     pthread_t th[THREAD_NUM];
     int i;
-    for (i = 0; i < THREAD_NUM; i++)
+    for (i = 0; i < THREAD_NUM; ++i)
     {
-        if (i > 0)
+        if (i >= 0)
         {
             if (pthread_create(&th[i], NULL, &producer, NULL) != 0)
             {
-                perror("Failed to create thread");
+                perror("Failed to create thread"); //checking to see if we failed to create a thread
             }
         }
     }
@@ -58,7 +61,7 @@ int main(int argc, char *argv[])
     {
         if (pthread_join(th[i], NULL) != 0)
         {
-            perror("Failed to join thread");
+            perror("Failed to join thread"); //checking to see if we failed to join a thread
         }
     }
     //sem_destroy(&shmp->semEmpty);
