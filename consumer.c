7f209121629f64fde67bf33a829b8b11ec038989
Lennice Bolton <76896866+DayBreak3681@users.noreply.github.com>
2023-11-07 23:55:09 -0500

    Update consumer.c

--- consumer.c
+++ consumer.c
@@ -1,3 +1,7 @@
+//Lennice Bolton
+// project 1 code.
+// 11/7/ 2023
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,62 +19,47 @@
 void *consumer(void *args)
 {
     size_t count = 0;
-    printf("beginning of consumer funtion");
     int fd;
     struct shmbuf *shmp;
-    fd = shm_open("/xyzabc",O_RDWR, 0600);
-    if(fd == -1){
-        printf("Error opening shared memory\n");
-    }
-    ftruncate(fd, sizeof(struct shmbuf));
-    shmp = mmap(NULL, sizeof(*shmp), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    while (1)
+    fd = shmget(HELLO,sizeof(struct shmbuf), 0600|IPC_CREAT); //shared memory
+    shmp = shmat(fd,NULL,0);
+     
+    int y;
+    for(int i = 0; i < 5; ++i)
     {
-        printf("In the while loop!\n");
-        int y;
         sem_wait(&shmp->semEmpty);
-        printf("called sem_wait\n");
-        y = shmp->buffer[count % 10];
-        printf("decrementing the count\n");
-        count++;
+        y = shmp->buffer[count];
+        //count++;
+        printf("consumed: %d\n", y);
         sem_post(&shmp->semFull);
         // Consume
-        printf("consuming\n");
         ++count;
-        printf("Got %d\n", y);
         sleep(1);
     }
 }
 
 int main(int argc, char *argv[])
 {
-    printf("In main\n");
     srand(time(NULL));
     pthread_t th[THREAD_NUM];
     int i;
 
     for (i = 0; i < THREAD_NUM; i++)
     {
-        printf("in first for loop");
         if (pthread_create(&th[i], NULL, &consumer, NULL) != 0)
         {
-            printf("In first if statement\n");
-            perror("Failed to create thread");
+            perror("Failed to create thread"); //checking to see if we failed to create a thread
         }
     }
     for (i = 0; i < THREAD_NUM; i++)
     {
-        printf("in second for loop\n");
         if (pthread_join(th[i], NULL) != 0)
         {
-            printf("in second if statement\n");
-            perror("Failed to join thread");
+            perror("Failed to join thread"); //checking to see if we failed to join a thread
         }
     }
     //sem_destroy(&shmp->semEmpty);
     //sem_destroy(&shmp->semFull);
-    shm_unlink("/xyzabc");
     exit(EXIT_SUCCESS);
-    printf("finished\n");
     return 0;
 }
